a887b780de9cd21aeb28f2c207a26c31aee7bd76
Pablo Orviz <orviz@ifca.unican.es>
2016-02-23 08:13:09 +0000

    Add python-pip to Ubuntu preparation.

--- bin/utils/prepare_ubuntu.sh
+++ bin/utils/prepare_ubuntu.sh
@@ -1,6 +1,6 @@
 apt-get -y install git
 git clone https://github.com/egi-qc/umd-verification.git
 
-apt-get -y install gcc python-pip python-dev python-ldap
+apt-get -y install gcc python-pip python-dev python-ldap python-pip
 pip install Fabric
 pip install -r umd-verification/requirements.txt
