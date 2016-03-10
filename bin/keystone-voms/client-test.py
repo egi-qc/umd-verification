013fcd131b9911515283b0e1aebfbd9da5a418e3
Pablo Orviz <orviz@ifca.unican.es>
2016-03-10 09:11:22 +0000

    Optional argument for passing output location of generated proxy.

--- bin/keystone-voms/client-test.py
+++ bin/keystone-voms/client-test.py
@@ -13,6 +13,8 @@ import novaclient.exceptions
 parser = argparse.ArgumentParser(description='Get keystone auth token.')
 parser.add_argument("tenant", metavar="TENANT|VO", type=str,
                     help="Tenant mapping the VO")
+parser.add_argument("--proxy-path", metavar="PATH", type=str,
+                    help="Path where the proxy is stored")
 args = parser.parse_args()
 
 
@@ -24,7 +26,10 @@ version = 2
 auth_system = "voms"
 novaclient.auth_plugin.discover_auth_systems()
 auth_plugin = novaclient.auth_plugin.load_plugin(auth_system)
-auth_plugin.opts["x509_user_proxy"] = os.environ["X509_USER_PROXY"]
+if args.proxy_path:
+    auth_plugin.opts["x509_user_proxy"] = args.proxy_path
+else:
+    auth_plugin.opts["x509_user_proxy"] = os.environ["X509_USER_PROXY"]
 
 client = novaclient.client.Client(version, username, password,
                                   tenant, url,
