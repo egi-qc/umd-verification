b7e5ae5af919d37598018a16eb5ebdfe34078a07
Pablo Orviz <orviz@ifca.unican.es>
2016-05-17 09:40:22 +0200

    Deploy python2.7 in SL6.

--- bin/utils/prepare_sl6.sh
+++ bin/utils/prepare_sl6.sh
@@ -6,11 +6,23 @@ yum -y install /tmp/epel-release-6-8.noarch.rpm
 yum -y install git
 git clone https://github.com/egi-qc/umd-verification.git
 
-yum -y install python-ldap gcc python-devel
+cat <<EOF >> /etc/yum.repos.d/puias-computational.repo
+[PUIAS_6_computational]
+name=PUIAS computational Base $releasever - $basearch
+mirrorlist=http://puias.math.ias.edu/data/puias/computational/$releasever/$basearch/mirrorlist
+#baseurl=http://puias.math.ias.edu/data/puias/computational/$releasever/$basearch
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puias
+EOF
+
+wget -q http://springdale.math.ias.edu/data/puias/6/x86_64/os/RPM-GPG-KEY-puias -O /etc/pki/rpm-gpg/RPM-GPG-KEY-puias
+rpm --import RPM-GPG-KEY-puias
+
+yum -y install gcc python27 python27-devel openldap-devel
 
 wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
-python /tmp/get-pip.py
+python2.7 /tmp/get-pip.py
 
 # Getting issues with upper versions
-pip install Fabric==1.8.1
-pip install -r umd-verification/requirements.txt
+pip2.7 install Fabric==1.8.1
+pip2.7 install -r umd-verification/requirements.txt
