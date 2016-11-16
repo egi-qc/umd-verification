2b079bfe0c77a7ad731842936c4be1d019136eae
Pablo Orviz <orviz@ifca.unican.es>
2016-11-16 16:02:17 +0000

    openstack_version gathered in runtime

--- etc/puppet/ooi.pp
+++ etc/puppet/ooi.pp
@@ -2,7 +2,6 @@ include umd
 
 class {
     "ooi":
-        openstack_version => "mitaka",
         manage_repos      => false,
         manage_service    => false,
         require           => Class["umd"],
