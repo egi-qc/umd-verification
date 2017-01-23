1796f8f2fbe52ec223d70cd6e6c10ad8ad18c926
Pablo Orviz <orviz@ifca.unican.es>
2017-01-23 12:22:22 +0000

    fts adapted to redux

--- etc/puppet/fts.pp
+++ etc/puppet/fts.pp
@@ -1,7 +1,68 @@
-include umd
+#Class['umd'] -> Class['fts::install'] -> Class['fts_mysql'] -> Class['fts::config'] -> Class['fts::service']
 
-class {
-    "fts":
-        require           => Class["umd"],
+include umd
+class {'fts::install': require => Class["umd"]}
+class {'fts_mysql': require => Class["fts::install"]}
+class {'fts::config': require => Class["fts_mysql"]}
+class {'fts::service': require => Class["fts::config"]}
+
+
+class fts_mysql {
+    # FIXME(orviz) use mysql module for these execs
+    $db_name = hiera("fts3_db_name")
+    $db_user = hiera("fts3_db_username")
+    $db_pass = hiera("fts3_db_password")
+    $schema  = hiera("fts_schema", "/usr/share/fts-mysql/fts-schema-1.0.0.sql")
+
+    if $::osfamily in ["Debian"] {
+        $pkg = ["mysql-server", "mod_ssl"]
+        $srv = "mysql"
+    }
+    elsif $::osfamily in ["RedHat", "CentOS"] {
+        $pkg = ["mariadb-server", "mod_ssl"]
+        $srv = "mariadb"
+    }
+    
+    package {
+        $pkg:
+            ensure => installed,
+    }
+    
+    service {
+        $srv:
+            enable  => true,
+            ensure  => running,
+            require => Package[$pkg],
+    }
+
+    exec {
+        "drop-db-if-exists":
+            command => "/usr/bin/mysql -e \"drop database IF EXISTS ftsdb\"",
+            require => Service[$srv]
+    }
+    exec {
+        "create-db":
+            command => "/usr/bin/mysql -e \"create database ${db_name}\"",
+            require => Exec["drop-db-if-exists"]
+    }
+    exec {
+        "import-db":
+            command => "/usr/bin/mysql ftsdb < $schema",
+            require => Exec["create-db"]
+    }
+    exec {
+        "grant-perms":
+            command => "/usr/bin/mysql -e \"GRANT ALL ON ${db_name}.* TO ${db_user}@'localhost' IDENTIFIED BY '${db_pass}';\"",
+            require => Exec["import-db"]
+    }
+    exec {
+        "flush-privileges":
+            command => "/usr/bin/mysql -e \"FLUSH PRIVILEGES;\"",
+            require => Exec["grant-perms"]
+    }
 }
+#class {
+#    "fts":
+#        require => [Class["umd"], Exec["flush-privileges"]]
+#}
 
--- umd/products/fts.py
+++ umd/products/fts.py
@@ -1,40 +1,8 @@
 from umd import base
 from umd.base.configure.puppet import PuppetConfig
-from umd import config
-from umd import system
-from umd import utils
 
 
-class FTSDeploy(base.Deploy):
-    def post_config(self):
-        hiera_config = utils.load_from_hiera(
-            config.CFG["cfgtool"].hiera_data)
-        db_name = hiera_config["fts3_db_name"]
-        db_user = hiera_config["fts3_db_username"]
-        db_pass = hiera_config["fts3_db_password"]
-
-        # mysql
-        if system.distro_version == "centos7":
-            pkg_server = "mariadb-server"
-            cmd_start = "systemctl start mariadb"
-        elif system.distro_version == "ubuntu14":
-            pkg_server = "mysql-server"
-            cmd_start = "service mysql start"
-        utils.install(pkg_server)
-        utils.runcmd(cmd_start, stop_on_error=False)
-        utils.runcmd("mysql -e \"drop database IF EXISTS ftsdb\"")
-        utils.runcmd("mysql -e \"create database %s\"" % db_name)
-        utils.runcmd("mysql ftsdb < /usr/share/fts-mysql/mysql-schema.sql")
-        utils.runcmd(("mysql -e \"GRANT ALL ON %s.* TO %s@'localhost' "
-                      "IDENTIFIED BY '%s';\""
-                     % (db_name, db_user, db_pass)))
-        utils.runcmd("mysql -e \"FLUSH PRIVILEGES;\"")
-
-        # httpd
-        utils.install("mod_ssl")
-
-
-fts = FTSDeploy(
+fts = base.Deploy(
     name="fts",
     doc="File Transfer Service (FTS) deployment.",
     need_cert=True,
