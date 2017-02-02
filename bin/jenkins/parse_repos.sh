5e2b6096ed62dd26ed14ece739eb8da0e3302d9a
Pablo Orviz <orviz@ifca.unican.es>
2017-02-02 16:05:26 +0000

    parse repos in multiline basis

--- bin/jenkins/parse_repos.sh
+++ bin/jenkins/parse_repos.sh
@@ -2,14 +2,14 @@
 #
 # $1 - Comma-separated string with the repository URLs
 
-IN="$(echo -e "${1}" | tr -d '[[:space:]]')"
-
 c=0
 repostr=''
-for i in $(echo $IN | tr "," "\n"); do
+for repo in "$@"; do
+    IN="$(echo -e "${repo}" | tr -d '[[:space:]]')"
+    echo $IN
     c=$((c+1))
     [ -n "$repostr" ] && repostr=$repostr','
-    repostr=$repostr"repository_file_$c=$i"
+    repostr=$repostr"repository_file_$c=$IN"
 done
 
 echo $repostr
