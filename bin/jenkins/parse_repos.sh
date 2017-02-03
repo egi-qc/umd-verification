5ab073684c88b21218ef64281b62f8d274855f06
Pablo Orviz <orviz@ifca.unican.es>
2017-02-03 08:49:36 +0100

    remove echo

--- bin/jenkins/parse_repos.sh
+++ bin/jenkins/parse_repos.sh
@@ -6,7 +6,6 @@ c=0
 repostr=''
 for repo in "$@"; do
     IN="$(echo -e "${repo}" | tr -d '[[:space:]]')"
-    echo $IN
     c=$((c+1))
     [ -n "$repostr" ] && repostr=$repostr','
     repostr=$repostr"repository_file_$c=$IN"
