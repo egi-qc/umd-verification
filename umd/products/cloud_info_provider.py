ca4d2ca50c1aaa048883b666528c31da6fe40567
Pablo Orviz <orviz@ifca.unican.es>
2017-02-06 16:07:12 +0100

    Return empty list if OS is not supported.

--- umd/products/cloud_info_provider.py
+++ umd/products/cloud_info_provider.py
@@ -3,4 +3,4 @@ from umd import system
 package_mapping = {
     "centos7": ["cloud-info-provider"],
     "ubuntu14": ["python-cloud-info-provider"]}
-package = package_mapping[system.distro_version]
+package = package_mapping.get(system.distro_version, [])
