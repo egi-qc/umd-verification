06360a011d9f74ca84d31542947ca84a34418336
Pablo Orviz <orviz@ifca.unican.es>
2017-02-15 15:12:56 +0000

    remove prints

--- bin/gridftp/client-test.sh
+++ bin/gridftp/client-test.sh
@@ -2,13 +2,10 @@
 
 set -x
 
-export
 export X509_USER_PROXY=$1
 
 subject=`sudo voms-proxy-info -acsubject -file $1`
 the_user=$SUDO_USER
-
-echo "the user: $SUDO_USER"
 
 sudo chown ${the_user}:${the_user} $1
 sudo bash -c "echo \"$subject $the_user\" >> /etc/grid-security/grid-mapfile"
