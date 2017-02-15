32d8666c98ac35b0d26af66c344c7a7384af37a9
Pablo Orviz <orviz@ifca.unican.es>
2017-02-15 12:10:31 +0000

    try to fix gridftp func test

--- bin/gridftp/client-test.sh
+++ bin/gridftp/client-test.sh
@@ -6,10 +6,12 @@ export
 export X509_USER_PROXY=$1
 
 subject=`sudo voms-proxy-info -acsubject -file $1`
-user=$SUDO_USER
+the_user=$SUDO_USER
 
-sudo chown ${user}:${user} $1
-sudo bash -c "echo \"$subject $user\" >> /etc/grid-security/grid-mapfile"
+echo "the user: $SUDO_USER"
+
+sudo chown ${the_user}:${the_user} $1
+sudo bash -c "echo \"$subject $the_user\" >> /etc/grid-security/grid-mapfile"
 
 globus-url-copy file:///etc/issue gsiftp://localhost:2811/tmp/issue
 diff /etc/issue /tmp/issue
