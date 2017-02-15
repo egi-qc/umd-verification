1a8d1a4340a41849ec0467c0c05552b2d38b9073
Pablo Orviz <orviz@ifca.unican.es>
2017-02-15 15:17:29 +0000

    correct user for running checks

--- bin/gridftp/client-test.sh
+++ bin/gridftp/client-test.sh
@@ -2,8 +2,13 @@
 
 set -x
 
-subject=`voms-proxy-info -acsubject`
-sudo bash -c "echo \"$subject $USER\" >> /etc/grid-security/grid-mapfile"
+export X509_USER_PROXY=$1
+
+subject=`sudo voms-proxy-info -acsubject -file $1`
+the_user=$SUDO_USER
+
+sudo chown ${the_user}:${the_user} $1
+sudo bash -c "echo \"$subject $the_user\" >> /etc/grid-security/grid-mapfile"
 
 globus-url-copy file:///etc/issue gsiftp://localhost:2811/tmp/issue
 diff /etc/issue /tmp/issue
