a9e1d4d59073ce26757da4cf96a225498b2fa447
Pablo Orviz <orviz@ifca.unican.es>
2017-05-18 16:29:55 +0200

    restart devstack nova-api

--- etc/puppet/ooi.pp
+++ etc/puppet/ooi.pp
@@ -6,3 +6,16 @@ class {
         manage_service    => false,
         require           => Class["umd"],
 }
+
+exec {
+    "Enable ooi API":
+        command => "/bin/sed -e '/enabled_apis*/c\enabled_apis = osapi_compute,metadata,ooi' /etc/nova/nova.conf",
+        require => Class["ooi"]
+}
+
+exec {
+    "Restart nova-api in devstack":
+        command => "pkill -9 -f nova-api && nohup python nova-api &",
+        path    => ["/usr/bin", "/usr/local/bin"],
+        require => Exec["Enable ooi API"]
+}
