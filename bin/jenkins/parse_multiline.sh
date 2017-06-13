d7b5a794c87d5b306ade1494ffe81fe578724044
Pablo Orviz <orviz@ifca.unican.es>
2017-06-13 11:03:23 +0000

    renamed

--- bin/jenkins/parse_multiline.sh
+++ bin/jenkins/parse_multiline.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 # $1 - Comma-separated string with the repository URLs
+# $2 - Argument name (prefix)
 
 c=0
 repostr=''
-for repo in "$@"; do
-    IN="$(echo -e "${repo}" | tr -d '[[:space:]]')"
+for i in "$@"; do
     c=$((c+1))
     [ -n "$repostr" ] && repostr=$repostr','
-    repostr=$repostr"repository_file_$c=$IN"
+    repostr=$repostr"${2}_$c=$i"
 done
 
 echo $repostr
