eb644fb1981dc0b132b15860b831029c95df6919
Pablo Orviz <orviz@ifca.unican.es>
2017-06-13 16:06:48 +0000

    missing ansible variables for configuring argus

--- umd/products/argus.py
+++ umd/products/argus.py
@@ -15,9 +15,22 @@ class ArgusDeploy(base.Deploy):
         # extra vars
         extra_vars = [
             "pap_host: %s" % system.fqdn,
+            "pap_port: 8150",
             "pap_host_dn: %s" % config.CFG["cert"].subject,
+            "pap_admin_dn: %s" % config.CFG["cert"].subject,
+            "pap_port_shutdown: 8151",
+            "pap_host_cert: /etc/grid-security/hostcert.pem",
+            "pap_host_key: /etc/grid-security/hostkey.pem",
             "pdp_host: %s" % system.fqdn,
-            "pepd_host: %s" % system.fqdn]
+            "pdp_port: 8152",
+            "pdp_port_admin: 8153",
+            "pdp_host_cert: /etc/grid-security/hostcert.pem",
+            "pdp_host_key: /etc/grid-security/hostkey.pem",
+            "pepd_host: %s" % system.fqdn,
+            "pepd_port: 8154",
+            "pepd_port_admin: 8155",
+            "pepd_host_cert: /etc/grid-security/hostcert.pem",
+            "pepd_host_key: /etc/grid-security/hostkey.pem"]
         self.cfgtool.extra_vars = extra_vars
 
 
