177dcb89ab8c6932c3a9586eea9b2f1947be8c93
Pablo Orviz <orviz@ifca.unican.es>
2017-06-13 11:32:59 +0000

    prefix as 1st argument

--- bin/jenkins/parse_multiline.sh
+++ bin/jenkins/parse_multiline.sh
@@ -3,12 +3,15 @@
 # $1 - Comma-separated string with the repository URLs
 # $2 - Argument name (prefix)
 
+prefix=$1
+shift
+
 c=0
 repostr=''
 for i in "$@"; do
     c=$((c+1))
     [ -n "$repostr" ] && repostr=$repostr','
-    repostr=$repostr"${2}_$c=$i"
+    repostr=$repostr"${prefix}_$c=$i"
 done
 
 echo $repostr
