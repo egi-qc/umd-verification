aaeebe14805a387fbd8b6aaa04c6f7fea0424665
Pablo Orviz <orviz@ifca.unican.es>
2017-06-14 13:17:16 +0200

    remove epel

--- bin/utils/prepare_sl6.sh
+++ bin/utils/prepare_sl6.sh
@@ -1,8 +1,5 @@
 yum -y install wget
 
-wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm  -O /tmp/epel-release-6-8.noarch.rpm
-yum -y install /tmp/epel-release-6-8.noarch.rpm
-
 yum -y install git
 git clone https://github.com/egi-qc/umd-verification.git
 
