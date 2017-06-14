958ecbe3920e0da8e562578e3f62a7bb5514a31f
Pablo Orviz <orviz@ifca.unican.es>
2017-06-14 13:13:53 +0200

    remove epel

--- bin/utils/prepare_centos7.sh
+++ bin/utils/prepare_centos7.sh
@@ -1,8 +1,5 @@
 yum -y install wget
 
-wget http://mirror.uv.es/mirror/fedora-epel//epel-release-latest-7.noarch.rpm -O /tmp/epel-release-latest-7.noarch.rpm
-yum -y install /tmp/epel-release-latest-7.noarch.rpm 
-
 yum -y install git
 git clone https://github.com/egi-qc/umd-verification.git
 
