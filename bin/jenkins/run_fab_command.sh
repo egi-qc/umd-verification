50cfb4e8626f2466b2cf78cef7e85e79f6d82484
Pablo Orviz <orviz@ifca.unican.es>
2017-07-21 12:23:38 +0000

    manage umd,cmd distros

--- bin/jenkins/run_fab_command.sh
+++ bin/jenkins/run_fab_command.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Args:
-#   $1: configuration management tool (ansible, puppet)
-#   $2: Role/module URL 
+#   $1: distribution and release (umd3, umd4, cmd1)
+#   $2: configuration management tool (ansible, puppet)
+#   $3: Role/module URL 
 
-module_name="`basename $2`"
+distro=$1
+config_tool=$2
+module_name="`basename $3`"
 
 ## sudo OR rvmsudo
 [[ $OS == sl6* ]] && sudocmd=rvmsudo || sudocmd=sudo
 
 ## ansible OR puppet
-case $1 in
+case $config_tool in
     *ansible*)
         $sudocmd pip install ansible==2.2
         module_path=/tmp/$module_name
@@ -23,6 +26,14 @@ case $1 in
         ;;
 esac
 
+# UMD or CMD
+case $distro in
+    umd3) release_str="umd_release=3" ;;
+    umd4) release_str="umd_release=4" ;;
+    cmd1) release_str="cmd_release=1" ;;
+    *) echo "UMD distribution '$distro' not known" && exit -1
+esac
+
 # fab execution
 Verification_repository=$(./bin/jenkins/parse_multiline.sh ${Verification_repository})
-$sudocmd fab argus:umd_release=${UMD_release},${Verification_repository},log_path=logs
+$sudocmd fab argus:umd_release=${release_str},${Verification_repository},log_path=logs
