f57227a2f2bf8e00b17d39274fbfd8f9c7e78972
Pablo Orviz <orviz@ifca.unican.es>
2017-08-03 12:22:20 +0200

    run the url-copy with appropriate user

--- bin/gridftp/client-test.sh
+++ bin/gridftp/client-test.sh
@@ -5,10 +5,10 @@ set -x
 export X509_USER_PROXY=$1
 
 subject=`sudo voms-proxy-info -acsubject -file $1`
-the_user=${SUDO_USER:-root}
-
+the_user=${SUDO_USER:-umd}
+[ -z "`getent passwd ${the_user}`" ] && sudo useradd -m umd
 sudo chown ${the_user}:${the_user} $1
 sudo bash -c "echo \"$subject $the_user\" >> /etc/grid-security/grid-mapfile"
 
-globus-url-copy file:///etc/issue gsiftp://localhost:2811/tmp/issue
+su $the_user -c "globus-url-copy file:///etc/issue gsiftp://localhost:2811/tmp/issue"
 diff /etc/issue /tmp/issue
