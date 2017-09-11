f177f712a83ea2c2e868d1b4000cc0351830de48
Pablo Orviz <orviz@ifca.unican.es>
2017-09-11 11:53:59 +0000

    uncomment ooi call

--- bin/ooi/occi.sh
+++ bin/ooi/occi.sh
@@ -1,5 +1,4 @@
 . ${1}/openrc
 
 export KID=`curl -i -s -H "Content-Type: application/json" -d '{"auth":{"identity":{"methods":["password"],"password":{"user":{"name":"demo","domain":{"id":"default"},"password":"secret"}}},"scope":{"project":{"name":"demo","domain":{"id":"default"}}}}}' ${OS_AUTH_URL}/v3/auth/tokens | grep "X-Subject-Token" | awk '{print $2}'`
-echo $KID
-#curl -v -H 'X-Auth-Token: '$KID -X GET localhost:8787/occi1.1/-/
+curl -v -H 'X-Auth-Token: '$KID -X GET localhost:8787/occi1.1/-/
