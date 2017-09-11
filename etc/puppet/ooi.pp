38ed42938b2b15b78b929d14108ac287ae28078c
Pablo Orviz <orviz@ifca.unican.es>
2017-09-11 08:21:51 +0000

    systemd configuration

--- etc/puppet/ooi.pp
+++ etc/puppet/ooi.pp
@@ -1,25 +1,67 @@
 class ooi_start {
+    $systemd_occi_str = "
+
+[Unit]
+Description = Devstack devstack@n-api-occi.service
+
+[Service]
+RestartForceExitStatus = 100
+NotifyAccess = all
+Restart = always
+KillSignal = SIGQUIT
+Type = notify
+ExecStart = /usr/bin/uwsgi --procname-prefix nova-api-occi --ini /etc/nova/nova-api-occi-uwsgi.ini
+User = stack
+SyslogIdentifier = devstack@n-api-occi.service
+
+[Install]
+WantedBy = multi-user.target
+    "
+    $uwsgi_init_str = "
+
+[uwsgi]
+http = :8787
+lazy-apps = true
+add-header = Connection: close
+buffer-size = 65535
+thunder-lock = true
+plugins = python
+enable-threads = true
+exit-on-reload = true
+die-on-term = true
+master = true
+processes = 2
+wsgi-file = /usr/bin/nova-api-wsgi
+    "
+
     exec {
         "Enable ooi API":
             command => "/bin/sed -i '/enabled_apis*/c\enabled_apis = osapi_compute,metadata,ooi' /etc/nova/nova.conf",
             require => Class["ooi"]
     }
-    
-    exec {
-        "kill nova-api":
-            command => "pkill -9 -f nova-api",
-            path    => ["/usr/bin", "/usr/local/bin"],
-            onlyif  => "pgrep -fc nova-api",
-            require => Exec["Enable ooi API"]
+ 
+    file {
+        "/etc/nova/nova-api-occi-uwsgi.ini":
+            content => $uwsgi_init_str
     }
-    
+ 
+    file {
+        "/etc/systemd/system/devstack\@n-api-occi.service":
+            content => $systemd_occi_str,
+            require => File["/etc/nova/nova-api-occi-uwsgi.ini"],
+            notify  => Exec["reload systemctl"]
+    }
+
     exec {
-        "Start nova-api":
-            command  => "/usr/local/bin/nova-api & echo $! >/opt/stack/status/stack/n-api.pid; fg || echo \"n-api failed to start\" | tee \"/opt/stack/status/stack/n-api.failure\"",
-            user     => "stack",
-            provider => shell,
-            path     => ["/usr/bin", "/usr/local/bin"],
-            require  => Exec["kill nova-api"]
+        "reload systemctl":
+            command   => "/bin/systemctl daemon-reload",
+            subscribe => File["/etc/systemd/system/devstack\@n-api-occi.service"]
+    }
+
+    exec {
+        "start devstack\@n-api-occi.service":
+            command => "/bin/systemctl restart "devstack@n-api-occi*",
+            require => Exec["reload systemctl"]
     }
 }
 
