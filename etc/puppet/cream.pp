955d7f8a847c9d720fbf5d6d4bb0932a1c8f184f
Pablo Orviz <orviz@ifca.unican.es>
2017-11-09 11:03:58 +0000

    add slurm support

--- etc/puppet/cream.pp
+++ etc/puppet/cream.pp
@@ -1,18 +1,5 @@
 class {"umd": before => [Class["gridftp::install"], Class["creamce::install"]]}
 
-## Uncomment for torque deployment
-#$torque_pkgs = [
-#    "torque-client",
-#    "torque-server"
-#]
-#package {
-#    $torque_pkgs:
-#        ensure => present
-#}
-#
-#$wns = {'host1' => { ip => '172.16.39.154'}}
-#create_resources(host, $wns)
-
 class {
     "creamce":
         require => [
@@ -20,4 +7,18 @@ class {
         ]
 }
 
-include slurm
+class slurmmaster::install {
+    if $::osfamily == "RedHat" {
+        $pkgs = {
+            "slurm-plugins" => { source => "https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-plugins-17.02.6-1.el7.centos.x86_64.rpm",
+                                 name   => "slurm-plugins", },
+            "slurm"         => { source => "https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-17.02.6-1.el7.centos.x86_64.rpm",
+                                 name   => "slurm", },
+            "slurm-munge"   => { source => "https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-munge-17.02.6-1.el7.centos.x86_64.rpm",
+                                 name   => "slurm-munge", },
+
+        }
+        create_resources(package, $pkgs)
+    }
+}
+include slurmmaster::install
