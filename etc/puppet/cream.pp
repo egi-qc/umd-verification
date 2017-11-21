68baa910711b50af25e7b5724b2972598fcbab61
Pablo Orviz <orviz@ifca.unican.es>
2017-11-21 16:20:03 +0000

    puppet for cream with slurm

--- etc/puppet/cream.pp
+++ etc/puppet/cream.pp
@@ -7,18 +7,4 @@ class {
         ]
 }
 
-class slurmmaster::install {
-    if $::osfamily == "RedHat" {
-        $pkgs = {
-            "slurm-plugins" => { source => "https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-plugins-17.02.6-1.el7.centos.x86_64.rpm",
-                                 name   => "slurm-plugins", },
-            "slurm"         => { source => "https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-17.02.6-1.el7.centos.x86_64.rpm",
-                                 name   => "slurm", },
-            "slurm-munge"   => { source => "https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-munge-17.02.6-1.el7.centos.x86_64.rpm",
-                                 name   => "slurm-munge", },
-
-        }
-        create_resources(package, $pkgs)
-    }
-}
-include slurmmaster::install
+include slurm
