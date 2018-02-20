52a5e59d4d510fc1f1efdf3b099375b4d2766970
Pablo Orviz <orviz@ifca.unican.es>
2018-02-20 14:49:34 +0000

    Create lcgdm-mapfile in advance.

--- umd/products/dpm.py
+++ umd/products/dpm.py
@@ -1,7 +1,18 @@
 from umd import base
 from umd.base.configure.puppet import PuppetConfig
+from umd import utils
 
-dpm = base.Deploy(
+
+class DPMDeploy(base.Deploy):
+    def pre_config(self):
+        dteam_dn = ("/DC=org/DC=terena/DC=tcs/C=ES/O=Consejo Superior de "
+                    "Investigaciones Cientificas/CN=PABLO ORVIZ FERNANDEZ "
+                    "594361@csic.es")
+        utils.write_to_file("/etc/lcgdm-mapfile",
+                            dteam_dn,
+                            mode='w')
+
+dpm = DPMDeploy(
     name="dpm",
     doc="DPM deployment using Puppet.",
     need_cert=True,
