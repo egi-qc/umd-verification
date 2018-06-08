24194597b7b5d6270310201f4f1a7dc15ea9accf
Pablo Orviz <orviz@ifca.unican.es>
2018-06-08 08:44:08 +0000

    fix

--- bin/arc/ansible-test.sh
+++ bin/arc/ansible-test.sh
@@ -9,7 +9,7 @@ cat <<'EOF' > $PLAYBOOK
 ---
 - hosts: localhost
   roles:
-    - role: ansible-arc-test
+    - role: ansible-arc
 EOF
 
 sudo ansible-playbook $PLAYBOOK --tags testing --extra-vars "enable_testing=true" --extra-vars "proxy_file=$PROXY_FILE"
