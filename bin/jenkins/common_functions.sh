3ebf63507fcce8be3d00b45f66b0198f08408faf
Pablo Orviz <orviz@ifca.unican.es>
2018-10-08 10:40:52 +0200

    Syntax fix

--- bin/jenkins/common_functions.sh
+++ bin/jenkins/common_functions.sh
@@ -281,10 +281,10 @@ archive_artifacts_in_workspace() {
         cp /tmp/Puppetfile "${WORKSPACE_CONFIG_DIR}/"
         mkdir "${WORKSPACE_CONFIG_DIR}/puppet"
         cp -r /etc/puppet/hiera.yaml /etc/puppet/hieradata "${WORKSPACE_CONFIG_DIR}/puppet"
-	mkdir "${WORKSPACE_CONFIG_DIR}/puppet/manifest"
+	    mkdir "${WORKSPACE_CONFIG_DIR}/puppet/manifest"
         cp etc/puppet/${MODULE} "${WORKSPACE_CONFIG_DIR}/puppet/manifest"
     elif [ $2 == "ansible" ]; then
-        mkdir ${WORKSPACE_CONFIG_DIR}/vars"
+        mkdir ${WORKSPACE_CONFIG_DIR}/vars
         cp /tmp/*.yaml "${WORKSPACE_CONFIG_DIR}/vars/"
     fi
 
@@ -310,7 +310,7 @@ publish_howtos () {
     git clone https://github.com/egi-qc/deployment-howtos /tmp/deployment-howtos && cd /tmp/deployment-howtos
     git remote set-url origin git@github.com:egi-qc/deployment-howtos.git
     ! [ -d ${FAB_CMD}/${OS} ] && mkdir -p ${FAB_CMD}/${OS}
-    cp -r "${WORKSPACE_CONFIG_DIR}/*" ${FAB_CMD}/${OS}/
+    cp -r ${WORKSPACE_CONFIG_DIR}/* ${FAB_CMD}/${OS}/
     git add ${FAB_CMD}/${OS}/
     git commit -a -m "${FAB_CMD}/${OS}/ deployment how-to (build $BUILD_URL)"
     git push origin master
