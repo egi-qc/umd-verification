b29fbc250c337099427a8486c93bc860a3778b92
Pablo Orviz <orviz@ifca.unican.es>
2019-11-08 10:21:25 +0100

    Make openstack_release optional

--- umd/base/configure/common.py
+++ umd/base/configure/common.py
@@ -16,23 +16,25 @@ from umd import utils as base_utils
 
 def set_umd_params(template_file, output_file):
     _distribution = config.CFG["distribution"]
-    if _distribution == "umd":
-        _release = config.CFG["umd_release"]
-    elif _distribution == "cmd":
-        _release = config.CFG["cmd_release"]
-    elif _distribution == "cmd-one":
-        _release = config.CFG["cmd_one_release"]
-
     _data = {
-        "release": _release,
         "distribution": _distribution,
         "repository_file": config.CFG.get("repository_file", ""),
-        "openstack_release": config.CFG.get("openstack_release", "False"),
         "igtf_repo": "False",
         "enable_testing_repo": config.CFG.get("enable_testing_repo", "False"),
         "enable_untested_repo": config.CFG.get("enable_untested_repo",
                                                "False"),
     }
+
+    if _distribution == "umd":
+        _release = config.CFG["umd_release"]
+    elif _distribution == "cmd":
+        _release = config.CFG["cmd_release"]
+        if config.CFG.get("openstack_release", False):
+            _data["openstack_release"] = config.CFG["openstack_release"]
+    elif _distribution == "cmd-one":
+        _release = config.CFG["cmd_one_release"]
+    _data["release"] = _release
+
     # if config.CFG.get("need_cert", ""):
     #     _data["igtf_repo"] = "True"
     base_utils.render_jinja(
