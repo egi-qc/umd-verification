708c43f26b308a6d7447ca9e562c2e8fd9777f35
Pablo Orviz <orviz@ifca.unican.es>
2024-02-27 13:04:17 +0100

    Update rc.py to enable testing repo

--- umd/products/rc.py
+++ umd/products/rc.py
@@ -20,7 +20,7 @@ from umd import config
 class ReleaseCandidateDeploy(base.Deploy):
     def pre_config(self):
         # extra vars
-        extra_vars = ["enable_candidate_repo: true",
+        extra_vars = ["enable_testing_repo: true",
                       "logfile: %s"
                       % os.path.join(config.CFG["log_path"], "install.log")]
         self.cfgtool.extra_vars = extra_vars
